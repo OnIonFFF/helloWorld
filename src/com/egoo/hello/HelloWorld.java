39e7302e4488aed3bea3b33d57e0e4fcaf2dca1b
柳志飞 <liuzhifei@egoonet.com>
2018-07-17 16:38:17 +0800

    修改代码之后，同步git项目

--- src/com/egoo/hello/HelloWorld.java
+++ src/com/egoo/hello/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("HelloWorld!");
+		System.out.print("上传项目到git后，随便添加修改");
 	}
 
 }
